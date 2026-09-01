6c23261c76d80ea3a4c7906e907d7fafdba9b823
newagebegins <newagebegins@gmail.com>
2026-09-02 06:14:17 +1000

    Add solution of day 23

---
+++ main.h
@@ -0,0 +1,46 @@
+#if !defined(MAIN_H)
+#define MAIN_H
+
+struct str
+{
+    u32 Length;
+    char *Text;
+};
+
+#define MAX_REGISTERS 4
+
+struct computer_state
+{
+    u32 InstructionIndex;
+    s32 Registers[MAX_REGISTERS];
+};
+
+enum instruction_type
+{
+    Instruction_cpy,
+    Instruction_inc,
+    Instruction_dec,
+    Instruction_jnz,
+    Instruction_tgl,
+};
+
+enum argument_type
+{
+    Argument_None,
+    Argument_Constant,
+    Argument_Register,
+};
+
+struct argument
+{
+    argument_type Type;
+    s32 Value;
+};
+
+struct instruction
+{
+    instruction_type Type;
+    argument Arguments[2];
+};
+
+#endif
