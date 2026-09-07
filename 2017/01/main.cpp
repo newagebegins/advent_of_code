aa7036c5edf4a056671f9af96be4dcc43d98c2d6
newagebegins <newagebegins@gmail.com>
2026-09-08 08:56:08 +1000

    2017-01 Part 2

--- 2017/01/main.cpp
+++ 2017/01/main.cpp
@@ -83,9 +83,43 @@ TestFindSumOfDigitsThatMatchTheNextDigit(char *Input, u32 Expected)
     FreeDigitList(&List);
 }
 
+internal u32
+FindSumOfDigitsThatMatchTheHalfwayAroundDigit(digit_list List)
+{
+    u32 Result = 0;
+    Assert((List.Count % 2) == 0);
+    u32 HalfwayOffset = List.Count / 2;
+    for(u32 Index = 0;
+        Index < List.Count;
+        ++Index)
+    {
+        u32 HalfwayIndex = Index + HalfwayOffset;
+        if(HalfwayIndex >= List.Count)
+        {
+            HalfwayIndex -= List.Count;
+        }
+        if(List.Digits[Index] == List.Digits[HalfwayIndex])
+        {
+            Result += List.Digits[Index];
+        }
+    }
+    return(Result);
+}
+
+internal void
+TestFindSumOfDigitsThatMatchTheHalfwayAroundDigit(char *Input, u32 Expected)
+{
+    digit_list List = CreateDigitList(Input);
+    u32 Result = FindSumOfDigitsThatMatchTheHalfwayAroundDigit(List);
+    Assert(Result == Expected);
+    FreeDigitList(&List);
+}
+
 int
 main(void)
 {
+    // NOTE(slava): Part 1
+
     TestFindSumOfDigitsThatMatchTheNextDigit("1122", 3);
     TestFindSumOfDigitsThatMatchTheNextDigit("1111", 4);
     TestFindSumOfDigitsThatMatchTheNextDigit("1234", 0);
@@ -94,4 +128,14 @@ main(void)
 
     char *Input = ReadEntireFileAndNullTerminate("input.txt");
     TestFindSumOfDigitsThatMatchTheNextDigit(Input, 1047);
+
+    // NOTE(slava): Part 2
+
+    TestFindSumOfDigitsThatMatchTheHalfwayAroundDigit("1212", 6);
+    TestFindSumOfDigitsThatMatchTheHalfwayAroundDigit("1221", 0);
+    TestFindSumOfDigitsThatMatchTheHalfwayAroundDigit("123425", 4);
+    TestFindSumOfDigitsThatMatchTheHalfwayAroundDigit("123123", 12);
+    TestFindSumOfDigitsThatMatchTheHalfwayAroundDigit("12131415", 4);
+
+    TestFindSumOfDigitsThatMatchTheHalfwayAroundDigit(Input, 982);
 }
