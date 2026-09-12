115ca99e7f520cc99b63df14f96ea2f27863de3d
newagebegins <newagebegins@gmail.com>
2026-09-12 11:24:14 +1000

    2017-04 Part 2

--- 2017/04/main.cpp
+++ 2017/04/main.cpp
@@ -90,8 +90,11 @@ GetWords(str Sentence, str *Words, u32 MaxWordCount)
     return(WordCount);
 }
 
-internal b32
-IsValidPassphrase(str Passphrase)
+#define IS_VALID_PASSPHRASE(name) b32 name(str Passphrase)
+typedef IS_VALID_PASSPHRASE(is_valid_passphrase);
+
+internal
+IS_VALID_PASSPHRASE(IsValidPassphrase)
 {
     b32 Result = true;
     str Words[16];
@@ -114,10 +117,80 @@ IsValidPassphrase(str Passphrase)
 }
 
 internal void
+CountLetters(str Word, u8 *Counts, u32 CountsCapacity)
+{
+    for(u32 CharIndex = 0;
+        CharIndex < Word.Length;
+        ++CharIndex)
+    {
+        char C = Word.Chars[CharIndex];
+        Assert((C >= 'a') && (C <= 'z'));
+        Assert((u32)C < CountsCapacity);
+        ++Counts[C];
+    }
+}
+
+internal b32
+AreAnagrams(str A, str B)
+{
+    b32 Result = false;
+    if(A.Length == B.Length)
+    {
+        Result = true;
+        u8 LetterCountA['z' + 1] = {};
+        u8 LetterCountB['z' + 1] = {};
+        CountLetters(A, LetterCountA, ArrayCount(LetterCountA));
+        CountLetters(B, LetterCountB, ArrayCount(LetterCountB));
+        for(char Letter = 'a';
+            Letter <= 'z';
+            ++Letter)
+        {
+            if(LetterCountA[Letter] != LetterCountB[Letter])
+            {
+                Result = false;
+                break;
+            }
+        }
+    }
+    return(Result);
+}
+
+internal
+IS_VALID_PASSPHRASE(IsValidPassphrase2)
+{
+    b32 Result = true;
+    str Words[16];
+    u32 WordCount = GetWords(Passphrase, Words, ArrayCount(Words));
+    for(u32 AIndex = 0;
+        (AIndex < WordCount) && Result;
+        ++AIndex)
+    {
+        for(u32 BIndex = AIndex + 1;
+            (BIndex < WordCount) && Result;
+            ++BIndex)
+        {
+            if(AreAnagrams(Words[AIndex], Words[BIndex]))
+            {
+                Result = false;
+            }
+        }
+    }
+    return(Result);
+}
+
+internal void
 TestIsValidPassphrase(char *Passphrase, b32 Expected)
 {
     str PassphraseStr = Str(Passphrase);
     b32 IsValid = IsValidPassphrase(PassphraseStr);
+    Assert(IsValid == Expected);
+}
+
+internal void
+TestIsValidPassphrase2(char *Passphrase, b32 Expected)
+{
+    str PassphraseStr = Str(Passphrase);
+    b32 IsValid = IsValidPassphrase2(PassphraseStr);
     Assert(IsValid == Expected);
 }
 
@@ -166,7 +239,7 @@ GetPassphrase(parser *Parser)
 }
 
 internal u32
-CountValidPassphrases(char *Input)
+CountValidPassphrases(char *Input, is_valid_passphrase IsValid)
 {
     u32 Result = 0;
     parser Parser = CreateParser(Input);
@@ -174,7 +247,7 @@ CountValidPassphrases(char *Input)
     while(Parsing(&Parser))
     {
         str Passphrase = GetPassphrase(&Parser);
-        if(IsValidPassphrase(Passphrase))
+        if(IsValid(Passphrase))
         {
             ++Result;
         }
@@ -188,10 +261,23 @@ main(void)
 {
     char *PuzzleInput = ReadEntireFileAndNullTerminate("input.txt");
 
+    // NOTE(slava): Part 1
+
     TestIsValidPassphrase("aa bb cc dd ee", true);
     TestIsValidPassphrase("aa bb cc dd aa", false);
     TestIsValidPassphrase("aa bb cc dd aaa", true);
 
-    u32 Count = CountValidPassphrases(PuzzleInput);
+    u32 Count = CountValidPassphrases(PuzzleInput, IsValidPassphrase);
     Assert(Count == 386);
+
+    // NOTE(slava): Part 2
+
+    TestIsValidPassphrase2("abcde fghij", true);
+    TestIsValidPassphrase2("abcde xyz ecdab", false);
+    TestIsValidPassphrase2("a ab abc abd abf abj", true);
+    TestIsValidPassphrase2("iiii oiii ooii oooi oooo", true);
+    TestIsValidPassphrase2("oiii ioii iioi iiio", false);
+
+    u32 Count2 = CountValidPassphrases(PuzzleInput, IsValidPassphrase2);
+    Assert(Count2 == 208);
 }
